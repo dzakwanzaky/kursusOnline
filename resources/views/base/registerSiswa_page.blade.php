92beb8ee5a8bb7917eb633ffd6de31c739dfa6c1
Riacup <supriyati98@mail.ugm.ac.id>
2019-11-06 19:18:43 +0700

    milih

--- resources/views/base/registerSiswa_page.blade.php
+++ resources/views/base/registerSiswa_page.blade.php
@@ -1,16 +1,21 @@
 @extends('base/script_page')
 @section('content')
     <div class="main">
-    <div class="container-content">
-        <div class="sign-up-content">
+    <div class="content">
+      <div class="container-fluid">
+        <div class="row">
+        <br>
+            <div class="col-md-12 col-md-offset-1">
+            <div class="card" style="display:block; margin-left:auto; margin-right:auto;">
+              <div class="card-body">
             <form method="POST" action="{{ route('register')}}" class="signup-form">
                 @csrf
 
                 <h2 class="form-title">Daftar Akun Imam Courses</h2>
                 <form action="/pendaftaranSiswa">
-                    <button class="button2" style="float:left">Murid</button>
+                    <button class="button" style="float:left">Murid</button>
                 <form action="#">
-                    <button class="button" disabled="disabled">Tutor</button>
+                    <button class="button2" disabled="disabled">Tutor</button>
                 </form>
                 <label for="nama">Nama Lengkap</label>
                 <div class="form-textbox">
@@ -69,7 +74,12 @@
             <p class="loginhere">
                 Sudah punya akun ?<a href="#" class="loginhere-link"> Masuk Sekarang</a>
             </p>
+            </div>
+            </div>
         </div>
+    </div>
+    </div>
+    </div>
     </div>
 
 </body><!-- This templates was made by Colorlib (https://colorlib.com) -->
