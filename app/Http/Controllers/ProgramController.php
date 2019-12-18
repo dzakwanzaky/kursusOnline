fdfda9b78ada57bb022b3f61abd8a962a328e136
fajarbay <fajarbay@github.com>
2019-12-18 10:56:43 +0700

    nav

--- app/Http/Controllers/ProgramController.php
+++ app/Http/Controllers/ProgramController.php
@@ -1,14 +1,11 @@
 <?php
-
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
 use App\ModelJadwal;
 use App\ModelSiswa;
 use App\ModelInvoice;
 use Auth;
 use PDF;
-
 class ProgramController extends Controller
 {
     /**
@@ -16,12 +13,10 @@ class ProgramController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-
     public function __construct()
     {
         $this->middleware('auth');
     }
-
     public function index()
     {
         $data = ModelJadwal::where('murid_id', '=', Auth::user()->id)->get();
@@ -29,7 +24,6 @@ class ProgramController extends Controller
         $invoice = ModelInvoice::where('murid_id', '=', Auth::user()->id)->get();
         return view('murid/invoicenya', compact('data', 'alamat', 'invoice'));
     }
-
     public function data()
     {
         $data = ModelJadwal::where('murid_id', '=', Auth::user()->id)->get();
@@ -37,13 +31,11 @@ class ProgramController extends Controller
         $invoice = ModelInvoice::where('murid_id', '=', Auth::user()->id)->get();
         return view('murid/invoice', compact('data', 'alamat', 'invoice'));
     }
-
     public function pdf()
     {
         $data = ModelJadwal::where('murid_id', '=', Auth::user()->id)->get();
         $alamat = ModelSiswa::where('id', '=', Auth::user()->id)->get();
         $invoice = ModelInvoice::where('murid_id', '=', Auth::user()->id)->get();
-
         $pdf = PDF::loadView('murid/invoicenya', ['data'=>$data, 'alamat'=>$alamat, 'invoice'=>$invoice])->setPaper('A4');
         return $pdf->stream('invoice');
     }
