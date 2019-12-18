34f446998164d658e63f105c8e46e2c8515df966
vaniaparamita <vania.parammita23@gmail.com>
2019-12-18 08:21:04 +0700

    Paket

--- resources/views/base/home_page.blade.php
+++ resources/views/base/home_page.blade.php
@@ -34,18 +34,16 @@
                                         <div class="pricing-table-header">
                                             <input id="murid_id" style="display:none" type="text" class="form-control" name="murid_id"
                                                     required autofocus>
-                                            <h2>PAKET REGULER</h2>
-                                            <h3>RP.2.000,00</h3>
+                                                    <h2>PAKET REGULER</h2>
+                                            <h3>FREE</h3>
                                         </div>
                                         <div class="pricing-table-space"></div>
-                                        <div class="pricing-table-text">
-                                        </div>
                                         <div class="pricing-table-features">
-                                            <p><i class="fa fa-envelope-o"></i> <strong>250</strong> Email Addresses</p>
-                                            <p><i class="fa fa-rocket"></i> <strong>125GB</strong> of Storage</p>
-                                            <p><i class="fa fa-database"></i> <strong>140</strong> Databases</p>
-                                            <p><i class="fa fa-link"></i> <strong>60</strong> Domains</p>
-                                            <p><i class="fa fa-life-ring"></i> <strong>24/7 Unlimited</strong> Support</p>
+                                            <p><i class="fa fa-envelope-o"></i> <strong>MATEMATIKA</strong>
+                                            <p><i class="fa fa-rocket"></i> <strong>IPA</strong>
+                                            <p><i class="fa fa-database"></i> <strong>BAHASA INDONESIA</strong>
+                                            <p><i class="fa fa-link"></i> <strong>BAHASA INGGRIS</strong>
+                                            <p><i class="fa fa-life-ring"></i> <strong>VIDEO MATERI</strong>    
                                         <br>
                                         </div>
                                     </div>
@@ -53,19 +51,20 @@
                                 <div class="col-md-4">
                                     <div class="pricing-table pricing-table-highlighted">
                                         <div class="pricing-table-header">
-                                            <h2>PAKET PREMIUM</h2>
-                                            <h3>Rp.2.000,00</h3>
+                                        <h2>PAKET PREMIUM</h2>
+                                            <h3>Rp.75.000,00</h3>
                                         </div>
                                         <div class="pricing-table-space"></div>
                                         <div class="pricing-table-text">
+                                            <p>Untuk Hidup yang Lebih Baik</p>
                                         </div>
                                         <div class="pricing-table-features">
-                                            <p><i class="fa fa-envelope-o"></i> <strong>150</strong> Email Addresses</p>
-                                            <p><i class="fa fa-rocket"></i> <strong>65GB</strong> of Storage</p>
-                                            <p><i class="fa fa-database"></i> <strong>60</strong> Databases</p>
-                                            <p><i class="fa fa-link"></i> <strong>30</strong> Domains</p>
-                                            <p><i class="fa fa-life-ring"></i> <strong>24/7 Unlimited</strong> Support</p>
-                                            <br>
+                                            <p><i class="fa fa-envelope-o"></i> <strong>MATEMATIKA</strong>
+                                            <p><i class="fa fa-rocket"></i> <strong>IPA</strong>
+                                            <p><i class="fa fa-database"></i> <strong>BAHASA INDONESIA</strong>
+                                            <p><i class="fa fa-link"></i> <strong>BAHASA INGGRIS</strong>
+                                            <p><i class="fa fa-life-ring"></i> <strong>SOAL DAN VIDEO MATERI</strong> 
+                                        <br>
                                         </div>
                                      </div>
                                 </div>
@@ -73,18 +72,16 @@
                                 <div class="col-md-4">
                                     <div class="pricing-table pricing-table-highlighted">
                                         <div class="pricing-table-header">
-                                            <h2>PAKET GOLD</h2>
-                                            <h3>RP.2.000,00</h3>
+                                        <h2>PAKET GOLD</h2>
+                                            <h3>RP.100.000,00</h3>
                                         </div>
                                         <div class="pricing-table-space"></div>
-                                        <div class="pricing-table-text">
-                                        </div>
                                         <div class="pricing-table-features">
-                                            <p><i class="fa fa-envelope-o"></i> <strong>250</strong> Email Addresses</p>
-                                            <p><i class="fa fa-rocket"></i> <strong>125GB</strong> of Storage</p>
-                                            <p><i class="fa fa-database"></i> <strong>140</strong> Databases</p>
-                                            <p><i class="fa fa-link"></i> <strong>60</strong> Domains</p>
-                                            <p><i class="fa fa-life-ring"></i> <strong>24/7 Unlimited</strong> Support</p>
+                                            <p><i class="fa fa-envelope-o"></i> <strong>MATEMATIKA</strong>
+                                            <p><i class="fa fa-rocket"></i> <strong>IPA</strong>
+                                            <p><i class="fa fa-database"></i> <strong>BAHASA INDONESIA</strong>
+                                            <p><i class="fa fa-link"></i> <strong>BAHASA INGGRIS</strong>
+                                            <p><i class="fa fa-life-ring"></i> <strong>SEMINAR, SOAL, DAN VIDEO MATERI</strong>
                                             <br>
                                         </div>
                                     </div>
