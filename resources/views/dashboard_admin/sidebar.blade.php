cd7af77a52f9e73395c0871974c44069547d1792
vaniaparamita <vania.parammita23@gmail.com>
2019-12-22 15:08:15 +0700

    pull

--- resources/views/dashboard_admin/sidebar.blade.php
+++ resources/views/dashboard_admin/sidebar.blade.php
@@ -44,6 +44,14 @@
               </p>
             </a>
           </li>
+          <li class="nav-item">
+            <a href="/manajemenTutor" class="nav-link ">
+              <i class="far fa-circle nav-icon"></i>
+              <p>
+                Manajemen Paket Program
+              </p>
+            </a>
+          </li>
           <li class="nav-item has-treeview {{ Request::segment(1) === 'list_pendaftaranSiswa' ? 'menu-open' : null }}
                   {{ Request::segment(1) === 'list_pendaftaranTutor' ? 'menu-open' : null }}">
             <a href="#" class="nav-link nav-link {{ Request::segment(1) === 'list_pendaftaranSiswa' ? 'nav-link active' : null }}
