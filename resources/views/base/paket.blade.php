e4d76bfa8ab54ef996bc146dfc331823b0a54a82
dzakwanzaky <39255214+dzakwanzaky@users.noreply.github.com>
2020-04-16 22:49:02 +0700

    merge

--- resources/views/base/paket.blade.php
+++ resources/views/base/paket.blade.php
@@ -1,16 +1,17 @@
    @include('base/header_page0')
-@extends('base/script_h')
-<head>
-<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet">
-<link rel="stylesheet" href="{{asset('/button')}}/paket.css">
-<link rel="stylesheet" href="{{asset('/button')}}/dropdown.css">
-<link rel="stylesheet" href="{{asset('/button')}}/owl.carousel.css">
-<link rel="stylesheet" href="{{asset('/button')}}/owl.theme.default.min.css">
+   @extends('base/script_h')
 
-</head>
-<bod>
-	<br>
-<!-- 
+   <head>
+       <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet">
+       <link rel="stylesheet" href="{{asset('/button')}}/paket.css">
+       <link rel="stylesheet" href="{{asset('/button')}}/dropdown.css">
+       <link rel="stylesheet" href="{{asset('/button')}}/owl.carousel.css">
+       <link rel="stylesheet" href="{{asset('/button')}}/owl.theme.default.min.css">
+
+   </head>
+   <bod>
+       <br>
+       <!-- 
 <div class="select">
    <select name="format" id="format">
       <option selected disabled>Pilih paket</option>
@@ -21,247 +22,75 @@
       <option value="mobi">mobi</option>
    </select>
 </div>
- -->	<section class="priching">
+ -->
+       @foreach($data as $d)
 
-		<section class="heading">Paket Bimbingan Belajar SD</section>
-		<div class="container">
-			<div class="row">
+       <section class="priching">
 
-	<div class="owl-carousel owl-theme">
-    @foreach($datas as $u)
-				<div class="col-sm-4">
-					<div class="price-table">
-						<div class="price-head">
-							<p>Paket</p>
-							<h4>{{$u->nama_paket}}</h4>
-							<h2><span class="supertext">Rp</span><h4i>{{$u->harga}}</h4i><span class="subtext">/Bulan</span></h2>
-						</div>
-						<div class="price-content">
-							<ul>
-								<li><span class="text">{{$u->jumlah_mapel}}&nbsp;</span>pilihan mapel</li>
-								<li><span class="text">60&nbsp;</span>menit durasi</li>
-								<li><span class="text">{{$u->jumlah_pertemuan}}&nbsp;</span>kali pertemuan</li>
-								
-							</ul>	
-						</div>
-					
-						<div class="price-bottom">
-							
-						</div>
-					</div>
-				</div>
+           <section class="heading">Paket Bimbingan Belajar {{$d->program}}</section>
+           <div class="container">
+               <div class="row">
+			   @foreach(($d->paket) as $d)
+                   <div class="owl-carousel owl-theme">
+				   <div class="col-sm-4">
+                    <div class="price-table">
+                        <div class="price-head">
+                            <p>Paket</p>
+                            <h4>{{$d->nama_paket}}</h4>
+                            <h2><span class="supertext">Rp</span>
+                                <h4i>{{$d->harga}}</h4i><span class="subtext">/Bulan</span>
+                            </h2>
+                        </div>
+                        <div class="price-content">
+                            <ul>
+                                <li><span class="text">{{$d->jumlah_mapel}}&nbsp;</span>pilihan mapel</li>
+                                <li><span class="text">60&nbsp;</span>menit durasi</li>
+                                <li><span class="text">{{$d->jumlah_pertemuan}}&nbsp;</span>kali pertemuan</li>
 
-@endforeach
+                            </ul>
+                        </div>
+                        <div class="price-bottom">
+                        </div>
+                    </div>
+                </div>
 
-
-
-    		</div>
-			</div>
-		</div>
-	</section>
-
-<section class="priching">
-		<section class="heading">Paket Bimbingan Belajar SMP</section>
-		<div class="container">
-			<div class="row">
-<div class="owl-carousel owl-theme">
-				@foreach($dats as $u)
-				<div class="col-sm-4">
-					<div class="price-table">
-						<div class="price-head">
-							<p>Paket</p>
-							<h4>{{$u->nama_paket}}</h4>
-							<h2><span class="supertext">Rp</span><h4i>{{$u->harga}}</h4i><span class="subtext">/Bulan</span></h2>
-						</div>
-						<div class="price-content">
-							<ul>
-								<li><span class="text">{{$u->jumlah_mapel}}&nbsp;</span>pilihan mapel</li>
-								<li><span class="text">60&nbsp;</span>menit durasi</li>
-								<li><span class="text">{{$u->jumlah_pertemuan}}&nbsp;</span>kali pertemuan</li>
-								
-							</ul>	
-						</div>
-					
-						<div class="price-bottom">
-							
-						</div>
-					</div>
-				</div>
-
-@endforeach
+                @endforeach
 
 
 
-			</div>
-		</div>
-	</section>
+                   </div>
+               </div>
+           </div>
+       </section>
 
 
-<section class="priching">
-		<section class="heading">Paket Bimbingan Belajar SMA</section>
-		<div class="container">
-			<div class="row">
-<div class="owl-carousel owl-theme">
-				@foreach($das as $v)
-				<div class="col-sm-4">
-					<div class="price-table">
-						<div class="price-head">
-							<p>Paket</p>
-							<h4>{{$v->nama_paket}}</h4>
-							<h2><span class="supertext">Rp</span><h4i>{{$v->harga}}</h4i><span class="subtext">/Bulan</span></h2>
-						</div>
-						<div class="price-content">
-							<ul>
-								<li><span class="text">{{$v->jumlah_mapel}}&nbsp;</span>pilihan mapel</li>
-								<li><span class="text">60&nbsp;</span>menit durasi</li>
-								<li><span class="text">{{$v->jumlah_pertemuan}}&nbsp;</span>kali pertemuan</li>
-								
-							</ul>	
-						</div>
-					
-						<div class="price-bottom">
-							
-						</div>
-					</div>
-				</div>
-
-@endforeach
+       @endforeach
 
 
 
-			</div>
-		</div>
-	</section>
+       </div>
+       </div>
+       </section>
 
-<section class="priching">
-		<section class="heading">Paket Bimbingan Belajar SBMPTN</section>
-		<div class="container">
-			<div class="row">
-<div class="owl-carousel owl-theme">
-				@foreach($sbm as $w)
-				<div class="col-sm-4">
-					<div class="price-table">
-						<div class="price-head">
-							<p>Paket</p>
-							<h4>{{$w->nama_paket}}</h4>
-							<h2><span class="supertext">Rp</span><h4i>{{$w->harga}}</h4i><span class="subtext">/Bulan</span></h2>
-						</div>
-						<div class="price-content">
-							<ul>
-								<li><span class="text">{{$w->jumlah_mapel}}&nbsp;</span>pilihan mapel</li>
-								<li><span class="text">60&nbsp;</span>menit durasi</li>
-								<li><span class="text">{{$w->jumlah_pertemuan}}&nbsp;</span>kali pertemuan</li>
-								
-							</ul>	
-						</div>
-					
-						<div class="price-bottom">
-							
-						</div>
-					</div>
-				</div>
+       <script src="{{asset('/button')}}/jquery.min.js" type="text/javascript"></script>
+       <script src="{{asset('/button')}}/owl.carousel.js" type="text/javascript"></script>
+       <script type="text/javascript">
+           $('.owl-carousel').owlCarousel({
+               loop: true,
+               margin: 10,
+               nav: false,
+               responsive: {
+                   0: {
+                       items: 1
+                   },
+                   600: {
+                       items: 3
+                   },
+                   1000: {
+                       items: 3
+                   }
+               }
+           })
+       </script>
 
-@endforeach
-
-
-
-			</div>
-		</div>
-	</section>
-
-
-
-<section class="priching">
-		<section class="heading">Paket Bimbingan Belajar CPNS</section>
-		<div class="container">
-			<div class="row">
-<div class="owl-carousel owl-theme">
-				@foreach($cpns as $z)
-				<div class="col-sm-4">
-					<div class="price-table">
-						<div class="price-head">
-							<p>Paket</p>
-							<h4>{{$z->nama_paket}}</h4>
-							<h2><span class="supertext">Rp</span><h4i>{{$z->harga}}</h4i><span class="subtext">/Bulan</span></h2>
-						</div>
-						<div class="price-content">
-							<ul>
-								<li><span class="text">{{$z->jumlah_mapel}}&nbsp;</span>pilihan mapel</li>
-								<li><span class="text">60&nbsp;</span>menit durasi</li>
-								<li><span class="text">{{$z->jumlah_pertemuan}}&nbsp;</span>kali pertemuan</li>
-								
-							</ul>	
-						</div>
-					
-						<div class="price-bottom">
-							
-						</div>
-					</div>
-				</div>
-
-@endforeach
-
-
-
-			</div>
-		</div>
-	</section>
-<section class="priching">
-		<section class="heading">Paket Bimbingan Belajar Komputer</section>
-		<div class="container">
-			<div class="row">
-<div class="owl-carousel owl-theme">
-				@foreach($komp as $m)
-				<div class="col-sm-4">
-					<div class="price-table">
-						<div class="price-head">
-							<p>Paket</p>
-							<h4>{{$m->nama_paket}}</h4>
-							<h2><span class="supertext">Rp</span><h4i>{{$m->harga}}</h4i><span class="subtext">/Bulan</span></h2>
-						</div>
-						<div class="price-content">
-							<ul>
-								<li><span class="text">{{$m->jumlah_mapel}}&nbsp;</span>pilihan mapel</li>
-								<li><span class="text">60&nbsp;</span>menit durasi</li>
-								<li><span class="text">{{$m->jumlah_pertemuan}}&nbsp;</span>kali pertemuan</li>
-								
-							</ul>	
-						</div>
-					
-						<div class="price-bottom">
-							
-						</div>
-					</div>
-				</div>
-
-@endforeach
-
-
-
-			</div>
-		</div>
-	</section>
-
-	<script src="{{asset('/button')}}/jquery.min.js" type="text/javascript"></script>
-<script src="{{asset('/button')}}/owl.carousel.js" type="text/javascript"></script>
-<script type="text/javascript">
-	$('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:false,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:3
-        },
-        1000:{
-            items:3
-        }
-    }
-})
-</script>
-
-</bod>
-
+   </bod>
